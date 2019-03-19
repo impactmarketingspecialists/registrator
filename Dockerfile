6457d2ca9c4ac1d9bdf77fcc77e38a3109235e17
Paul Cantea <pcantea@gmail.com>
2019-03-18 19:44:22 -0700

    Fix for DEP issues

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM alpine:3.8 AS builder
 COPY . /go/src/github.com/impactmarketing/registrator
 
+ADD keys/id_rsa /root/.ssh/id_rsa
+RUN chmod 700 /root/.ssh/id_rsa
+
 RUN apk --no-cache add -t build-deps build-base go git curl \
+    && echo "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config \
+    && git config --global url.ssh://git@github.com/.insteadOf https://github.com/ \
 	&& apk --no-cache add ca-certificates \
 	&& export GOPATH=/go && mkdir -p /go/bin && export PATH=$PATH:/go/bin \
 	&& curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh \
--- modules.go
+++ modules.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	_ "github.com/gliderlabs/registrator/consul"
-	_ "github.com/gliderlabs/registrator/consulkv"
-	_ "github.com/gliderlabs/registrator/etcd"
-	_ "github.com/gliderlabs/registrator/skydns2"
-	_ "github.com/gliderlabs/registrator/zookeeper"
+	_ "github.com/impactmarketingspecialists/registrator/consul"
+	_ "github.com/impactmarketingspecialists/registrator/consulkv"
+	_ "github.com/impactmarketingspecialists/registrator/etcd"
+	_ "github.com/impactmarketingspecialists/registrator/skydns2"
+	_ "github.com/impactmarketingspecialists/registrator/zookeeper"
 )
